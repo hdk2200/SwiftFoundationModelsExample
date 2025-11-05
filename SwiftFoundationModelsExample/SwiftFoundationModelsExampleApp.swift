dca0d4512ddfd3ca54bee24dea7b5683de411eb5
H.K <16552133+hdk2200@users.noreply.github.com>
2025-11-05 17:26:54 +0900

    first commit

---
+++ SwiftFoundationModelsExample/SwiftFoundationModelsExampleApp.swift
@@ -0,0 +1,15 @@
+//
+// Copyright (c) 2025, ___ORGANIZATIONNAME___ All rights reserved. 
+// 
+//
+
+import SwiftUI
+
+@main
+struct SwiftFoundationModelsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
